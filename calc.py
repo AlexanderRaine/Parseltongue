8986bdf00f2363a872f1baadaca2e3ac89cf22a4
AlexanderRaine <kkalexlays@gmail.com>
2021-01-14 20:11:02 +0300

    First steps

---
+++ calc.py
@@ -0,0 +1,11 @@
+what = input("Выбор +- ")
+a = float(input("Число А"))
+b = float(input("Число Б"))
+c = 0
+if what == "+":
+    c = a + b
+elif what == "-":
+    c = a - b
+else:
+    print("Zatrahalo")
+print(c)
